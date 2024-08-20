b1e00dd141d28f33fe6f50de2ebcca2ee9ce03bc
DanielWallman-Dev <danielwallmanld@gmail.com>
2024-08-20 11:58:08 +0200

    Added a cell file for the cell class to draw cells and updated main to test various cases of drawing the cells

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-from graphics import Window, Line, Point
+from graphics import Window
+from cell import Cell
 
 def main():
     win = Window(800, 600)
-    l = Line(Point(50, 50), Point(400, 400))
-    win.draw_line(l, "black")
+
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 125, 125)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(135, 135, 225, 225)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(245, 245, 300, 300)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(350, 350, 500, 500)
+
     win.wait_for_close()
 
 
