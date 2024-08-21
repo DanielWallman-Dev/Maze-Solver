062eb141ce807e90398b06a20cb1cd0f73b5ea12
DanielWallman-Dev <danielwallmanld@gmail.com>
2024-08-21 15:36:03 +0200

    Added the ability to break down walls, currently only top and bottom for entrance and exit and created some tests for it

--- tests.py
+++ tests.py
@@ -41,5 +41,31 @@ class Tests(unittest.TestCase):
             num_rows,
         )
 
+    def test_maze_break_and_exit(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            m1._cells[0][0].has_top_wall,
+            False,
+        )
+        self.assertEqual(
+            m1._cells[num_cols - 1][num_rows - 1].has_bottom_wall,
+            False,
+        )
+
+    def test_maze_break_and_exit_true(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            m1._cells[0][2].has_top_wall,
+            True,
+        )
+        self.assertEqual(
+            m1._cells[num_cols - 2][num_rows - 2].has_bottom_wall,
+            True,
+        )
+
 if __name__ == "__main__":
     unittest.main()
