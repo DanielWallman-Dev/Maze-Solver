be7c2e331547959f9b4d307733fed79dcaef08d6
DanielWallman-Dev <danielwallmanld@gmail.com>
2024-08-21 16:54:31 +0200

    Created randomness in the ways the walls are removed between cells to create a maze structure and added some tests to test if the walls were removed/reset when they should

--- maze.py
+++ maze.py
@@ -1,9 +1,9 @@
 from cell import Cell
-#import random
+import random
 import time
 
 class Maze:
-    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None):
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None, seed=None):
         self._cells = []
         self._x1 = x1
         self._y1 = y1
@@ -12,9 +12,13 @@ class Maze:
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win
+        if seed:
+            random.seed(seed or 0)
 
         self._create_cells()
         self._break_entrance_and_exit()
+        self._break_walls_r(0, 0)
+        self._reset_cells_visited()
 
     def _create_cells(self):
         for i in range(self._num_cols):
@@ -48,3 +52,63 @@ class Maze:
         self._draw_cell(0, 0)
         self._cells[self._num_cols - 1][self._num_rows - 1].has_bottom_wall = False
         self._draw_cell(self._num_cols - 1, self._num_rows - 1)
+
+    def _break_walls_r(self, i, j):
+        self._cells[i][j].visited = True
+        while True:
+            next_to_visit_list = []
+
+            #cells to visit next
+            #left
+            if i > 0 and not self._cells[i - 1][j].visited:
+                next_to_visit_list.append((i - 1, j))
+            
+            #right
+            if i < self._num_cols - 1 and not self._cells[i + 1][j].visited:
+                next_to_visit_list.append((i + 1, j))
+
+            #up
+            if j > 0 and not self._cells[i][j - 1].visited:
+                next_to_visit_list.append((i, j - 1))
+
+            #down
+            if j < self._num_rows - 1 and not self._cells[i][j + 1].visited:
+                next_to_visit_list.append((i, j + 1))
+
+            #if there are no more directions, draw the current cell
+            if len(next_to_visit_list) == 0:
+                self._draw_cell(i, j)
+                return
+            
+            #pick a random direction
+            rand_direction = random.randrange(len(next_to_visit_list))
+            next_to_visit = next_to_visit_list[rand_direction]
+
+            #knock out the walls between current and next cells 
+            #right
+            if next_to_visit[0] == i + 1:
+                self._cells[i][j].has_right_wall = False
+                self._cells[i + 1][j].has_left_wall = False
+
+            #left
+            if next_to_visit[0] == i - 1:
+                self._cells[i][j].has_left_wall = False
+                self._cells[i - 1][j].has_right_wall = False
+
+            #up
+            if next_to_visit[1] == j - 1:
+                self._cells[i][j].has_top_wall = False
+                self._cells[i][j - 1].has_bottom_wall = False
+
+            #down
+            if next_to_visit[1] == j + 1:
+                self._cells[i][j].has_bottom_wall = False
+                self._cells[i][j + 1].has_top_wall = False
+
+            #recursive call for next cell
+            self._break_walls_r(next_to_visit[0], next_to_visit[1])
+
+    def _reset_cells_visited(self):
+        for col in self._cells:
+            for cell in col:
+                cell.visited = False
--- tests.py
+++ tests.py
@@ -59,13 +59,28 @@ class Tests(unittest.TestCase):
         num_rows = 10
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
         self.assertEqual(
-            m1._cells[0][2].has_top_wall,
+            m1._cells[1][0].has_top_wall,
             True,
         )
         self.assertEqual(
-            m1._cells[num_cols - 2][num_rows - 2].has_bottom_wall,
+            m1._cells[num_cols - 2][num_rows - 1].has_bottom_wall,
             True,
         )
+
+    def test_maze_reset_cells_visited(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        for col in m1._cells:
+            for cell in col:
+                cell.visited = True
+        m1._reset_cells_visited()
+        for col in m1._cells:
+            for cell in col:
+                self.assertEqual(
+                    cell.visited,
+                    False,
+                )
 
 if __name__ == "__main__":
     unittest.main()
